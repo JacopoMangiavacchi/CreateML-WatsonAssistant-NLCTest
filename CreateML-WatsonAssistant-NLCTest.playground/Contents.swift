810e788d0eab2477693718873b685c1d08f906bc
Jacopo <jacopo@jacopos-mac.local>
2018-06-05 16:53:54 -0700

    export CoreML model

--- CreateML-WatsonAssistant-NLCTest.playground/Contents.swift
+++ CreateML-WatsonAssistant-NLCTest.playground/Contents.swift
@@ -43,13 +43,33 @@ if let fileURL = Bundle.main.url(forResource: "dataset", withExtension: "json") 
         let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
         
 
-        print(trainingAccuracy, validationAccuracy, evaluationMetrics, evaluationAccuracy)
+        print("\ntrainingAccuracy = \(trainingAccuracy)")
+        print("validationAccuracy = \(validationAccuracy)")
+        print("evaluationAccuracy = \(evaluationAccuracy)")
+        print("evaluationMetrics\n", evaluationMetrics)
+
+        let predictionsTest =  ["what time is it",
+                                "play some rock music",
+                                "turn off the radio",
+                                "I'm hungry",
+                                "is it going to rain",
+                                "search for restaurant",
+                                "show me where to eat nearby"]
+        
+        let predictionResponse = try watsonClassifier.predictions(from: predictionsTest)
+
+        var i = 0
+        for response in predictionResponse {
+            print("\(predictionsTest[i]) => \(response)")
+            i += 1
+        }
 
         let metadata = MLModelMetadata(author: "Jacopo Mangiavacchi",
                                        shortDescription: "Watson Assistant Car demo intent classifier",
                                        version: "1.0")
         
-        try watsonClassifier.write(to: URL(fileURLWithPath: "CarWatson.mlmodel"), metadata: metadata)
+        try watsonClassifier.write(toFile: "/Users/jacopo/Desktop/CreateML-WatsonAssistant-NLCTest/carwatson.mlmodel", metadata: metadata)
+        
     }
     catch let error {
         print("\(error.localizedDescription)")
