e279b2316b7bd781a15b0f24bffc6dc1c205447f
Jacopo <jacopo@jacopos-mac.local>
2018-06-05 15:36:07 -0700

    Importing Watson dataset

--- CreateML-WatsonAssistant-NLCTest.playground/Contents.swift
+++ CreateML-WatsonAssistant-NLCTest.playground/Contents.swift
@@ -1,2 +1,25 @@
 import Foundation
 
+public struct Intent: Codable {
+    let intent: String
+    let utterances: [String]
+}
+
+public struct Dataset : Codable {
+    let intents: [Intent]
+}
+
+if let fileURL = Bundle.main.url(forResource: "dataset", withExtension: "json") {
+    do {
+        let data = try Data(contentsOf: fileURL)
+        let dataset = try JSONDecoder().decode(Dataset.self, from: data)
+
+
+        print(dataset)
+
+    }
+    catch let error {
+        print("\(error.localizedDescription)")
+    }
+}
+
