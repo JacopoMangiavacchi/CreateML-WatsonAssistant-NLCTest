f89422c02a156349e7b110e6f9e0cf96b9732785
Jacopo <jmangia@me.com>
2018-06-05 12:00:40 -0700

    Initial commit

---
+++ CreateML-WatsonAssistant-NLCTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CreateML-WatsonAssistant-NLCTest
+//
+//  Created by Jacopo Mangiavacchi on 6/5/18.
+//  Copyright © 2018 Jacopo Mangiavacchi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
